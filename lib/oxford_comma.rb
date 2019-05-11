3a7fa974d8187607ac4ed9613545f1c2a46d2507
Haley DeKeyser <hcd09@live.com>
2019-05-11 23:36:40 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -6,7 +6,7 @@ def oxford_comma(array)
     when x == 2
       array.join(" and ")
     when x == 3
-      array[0..1].join(", ") + "and " + array.last
+      array[0..2].join(", ") + "and " + array.last
     when x > 3
       array[0..(x - 1)].join(", ") + "and " + array.last
   end
