6beef4f725c9b581b3a451eab0221b13809c90c1
Haley DeKeyser <hcd09@live.com>
2019-05-11 23:29:14 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,11 +2,8 @@ def oxford_comma(array)
   x = array.length
   case x
     when x == 1
-      puts array.join
+      array.join
     when x == 2
-      puts array.join(" and ")
-    when x == 3
-    else
-      
+      array.join(" and ")
   end
 end
